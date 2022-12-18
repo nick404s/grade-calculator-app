beae33c9104d1bf7f468c999e27f51ebc31dc201
Nikolay Skryabin <nickskryabin@gmail.com>
2022-12-18 12:15:43 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace GradeCalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ScoresRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.StudentPictureBox = new System.Windows.Forms.PictureBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FinalGradeLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.ClassLabel = new System.Windows.Forms.Label();
+            this.SemesterLabel = new System.Windows.Forms.Label();
+            this.PalomarLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentPictureBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Title = "Open";
+            // 
+            // ScoresRichTextBox
+            // 
+            this.ScoresRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ScoresRichTextBox.Cursor = System.Windows.Forms.Cursors.No;
+            this.ScoresRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoresRichTextBox.Location = new System.Drawing.Point(89, 325);
+            this.ScoresRichTextBox.Name = "ScoresRichTextBox";
+            this.ScoresRichTextBox.ReadOnly = true;
+            this.ScoresRichTextBox.Size = new System.Drawing.Size(166, 205);
+            this.ScoresRichTextBox.TabIndex = 3;
+            this.ScoresRichTextBox.TabStop = false;
+            this.ScoresRichTextBox.Text = "";
+            // 
+            // StudentPictureBox
+            // 
+            this.StudentPictureBox.Location = new System.Drawing.Point(384, 79);
+            this.StudentPictureBox.Name = "StudentPictureBox";
+            this.StudentPictureBox.Size = new System.Drawing.Size(182, 199);
+            this.StudentPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.StudentPictureBox.TabIndex = 4;
+            this.StudentPictureBox.TabStop = false;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Enabled = false;
+            this.CalculateButton.Location = new System.Drawing.Point(407, 431);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(138, 34);
+            this.CalculateButton.TabIndex = 5;
+            this.CalculateButton.Text = "Calculate Grade";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(427, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Final Grade";
+            // 
+            // FinalGradeLabel
+            // 
+            this.FinalGradeLabel.AutoSize = true;
+            this.FinalGradeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FinalGradeLabel.Location = new System.Drawing.Point(458, 371);
+            this.FinalGradeLabel.Name = "FinalGradeLabel";
+            this.FinalGradeLabel.Size = new System.Drawing.Size(0, 31);
+            this.FinalGradeLabel.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Class:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Semester:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(85, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Scores";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(101, 99);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(0, 24);
+            this.NameLabel.TabIndex = 12;
+            // 
+            // ClassLabel
+            // 
+            this.ClassLabel.AutoSize = true;
+            this.ClassLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClassLabel.Location = new System.Drawing.Point(101, 162);
+            this.ClassLabel.Name = "ClassLabel";
+            this.ClassLabel.Size = new System.Drawing.Size(0, 24);
+            this.ClassLabel.TabIndex = 13;
+            // 
+            // SemesterLabel
+            // 
+            this.SemesterLabel.AutoSize = true;
+            this.SemesterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SemesterLabel.Location = new System.Drawing.Point(101, 231);
+            this.SemesterLabel.Name = "SemesterLabel";
+            this.SemesterLabel.Size = new System.Drawing.Size(0, 24);
+            this.SemesterLabel.TabIndex = 14;
+            // 
+            // PalomarLabel
+            // 
+            this.PalomarLabel.AutoSize = true;
+            this.PalomarLabel.Font = new System.Drawing.Font("Verdana", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PalomarLabel.ForeColor = System.Drawing.Color.Magenta;
+            this.PalomarLabel.Location = new System.Drawing.Point(12, 67);
+            this.PalomarLabel.Name = "PalomarLabel";
+            this.PalomarLabel.Size = new System.Drawing.Size(34, 32);
+            this.PalomarLabel.TabIndex = 15;
+            this.PalomarLabel.Text = "P";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(591, 27);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenToolStripMenuItem,
+            this.ExitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(41, 23);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // OpenToolStripMenuItem
+            // 
+            this.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem";
+            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(112, 24);
+            this.OpenToolStripMenuItem.Text = "Open";
+            this.OpenToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(112, 24);
+            this.ExitToolStripMenuItem.Text = "Exit";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(591, 566);
+            this.ControlBox = false;
+            this.Controls.Add(this.PalomarLabel);
+            this.Controls.Add(this.SemesterLabel);
+            this.Controls.Add(this.ClassLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FinalGradeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.StudentPictureBox);
+            this.Controls.Add(this.ScoresRichTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Grade Calculator App";
+            ((System.ComponentModel.ISupportInitialize)(this.StudentPictureBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox ScoresRichTextBox;
+        private System.Windows.Forms.PictureBox StudentPictureBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label FinalGradeLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label ClassLabel;
+        private System.Windows.Forms.Label SemesterLabel;
+        private System.Windows.Forms.Label PalomarLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,320 @@
+﻿/*
+ * School: Palomar College
+ * Course: CSIT 180
+ * Project: C# Project 6
+ * Student: Nikolay Skryabin 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GradeCalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        // variables to store student's data
+        private string name;
+        private string className;
+        private string semester;
+        private string pictureFileName;
+        private double[] scores;
+
+        // a variable to store a file path
+        private string filePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // initialize the variables
+            name = string.Empty;
+            className = string.Empty;
+            semester = string.Empty;
+            pictureFileName = string.Empty;
+            filePath = String.Empty;
+            scores = new double[8];
+
+            // set the Palomar College label text
+            PalomarLabel.Text = "P\nA\nL\nO\nM\nA\nR\n\nC\nO\nL\nL\nE\nG\nE";
+        }
+
+        /// <summary>
+        /// Exits the App from the menu.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Opens file dialog from the menu and processes the selected file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // set initial directory to open for choosing the student files
+            openFileDialog1.InitialDirectory = Application.CommonAppDataPath + @"\Static\";
+
+            // disable multiselecting files
+            openFileDialog1.Multiselect = false;
+
+            // select only text files
+            openFileDialog1.Filter = "Text files|*.txt";
+
+            // restore each time
+            openFileDialog1.RestoreDirectory = true;
+
+            // open the window
+            DialogResult dialog = openFileDialog1.ShowDialog();
+
+            // check if a file was selected
+            if (dialog == DialogResult.OK)
+            {
+                // clear the grade
+                FinalGradeLabel.Text = string.Empty;
+
+                // enable the calculate button
+                CalculateButton.Enabled = true;
+
+                // process data from the selected file
+                ProcessFileData();
+
+                // set the data in the UI
+                SetStudentInfo();
+            }
+        }
+
+        /// <summary>
+        /// Processes the selected file data.
+        /// Throws an Exception if the processed data type is not correct.
+        /// </summary>
+        private void ProcessFileData()
+        {
+            // get an array with a student data
+            string[] data = ReadData().Split(',');
+
+            // initialize the variables with the data 
+            name = data[0];
+            className = data[1];
+            semester = data[2];
+            pictureFileName = data[3];
+
+            // initialize index for processing scores
+            int index = 0;
+
+            try
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    // check for elements at indexes after 3
+                    if (i > 3)
+                    {
+                        // initialize the scores array
+                        scores[index++] = Convert.ToDouble(data[i]);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                // Display the error message
+                DisplayError(exception);
+            }
+        }
+
+        /// <summary>
+        /// Reads data from the selected file. 
+        /// Throws an Exception if the file is not found.
+        /// </summary>
+        /// <returns>a string with data</returns>
+        private string ReadData()
+        {
+            // a string to store a student data from file
+            string line = string.Empty;
+
+            try
+            {
+                // get the file path
+                filePath = openFileDialog1.FileName;
+
+                // create the stream reader and pass the path
+                StreamReader reader = new StreamReader(filePath);
+
+                // read the file till end
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                }
+
+                reader.Close();
+            }
+            catch (Exception exception)
+            {
+                // Display the error message
+                DisplayError(exception);
+            }
+            return line;
+        }
+
+        /// <summary>
+        /// Sets a student info and image.
+        /// Throws an Exception if the image is not found.
+        /// </summary>
+        private void SetStudentInfo()
+        {
+            try
+            {
+                // set the labels, picture, scores
+                NameLabel.Text = name;
+                ClassLabel.Text = className;
+                SemesterLabel.Text = semester;
+                StudentPictureBox.Image = Image.FromFile($@"Images\{pictureFileName}");
+                SetScores();
+            }
+            catch (Exception exception)
+            {
+                // Display the error message
+                DisplayError(exception);
+            }
+        }
+
+        /// <summary>
+        /// Sets student's scores.
+        /// </summary>
+        private void SetScores()
+        {
+            // clear the text box
+            ScoresRichTextBox.Text = String.Empty;
+
+            // loop to add scores to the textbox
+            for (int i = 0; i < scores.Length; i++)
+            {
+                // check projects
+                if (i < scores.Length - 2)
+                {
+                    ScoresRichTextBox.AppendText($"{$"Project{i + 1}",-15} {scores[i]}\n");
+                }
+                // check midterm
+                if (i == scores.Length - 2)
+                {
+                    ScoresRichTextBox.AppendText($"{"Midterm",-15}{scores[i]}\n");
+                }
+                // check final
+                if (i == scores.Length - 1)
+                {
+                    ScoresRichTextBox.AppendText($"{"Final",-15}    {scores[i]}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calculates student's grade.
+        /// Throws an Exception if the scores are not present.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FinalGradeLabel.Text = GetGradeAndColor().ElementAt(0).Key;
+                FinalGradeLabel.ForeColor = GetGradeAndColor().ElementAt(0).Value;
+            }
+            catch (Exception exception)
+            {
+                // Display the error message
+                DisplayError(exception);
+
+            }
+        }
+
+        /// <summary>
+        /// Returns the color and letter grade depending on 
+        /// a student score.
+        /// </summary>
+        /// <returns>dictionary with the grade and color</returns>
+        private Dictionary<string, Color> GetGradeAndColor()
+        {
+            // get points 
+            double points = GetPoints();
+
+            // check the points and return a Dictionary
+            // with the letter and color 
+            if (points > 89)
+            {
+                return new Dictionary<string, Color>() 
+                {
+                    { "A", Color.Green }
+                };
+            }
+            else if (points > 79)
+            {
+                return new Dictionary<string, Color>()
+                {
+                    { "B", Color.Green }
+                };
+            }
+            else if (points > 69)
+            {
+                return new Dictionary<string, Color>()
+                {
+                    { "C", Color.Green }
+                };
+            }
+            else if (points > 59)
+            {
+                return new Dictionary<string, Color>()
+                {
+                    { "D", Color.Orange }
+                };
+            }
+            else
+            {
+                return new Dictionary<string, Color>()
+                {
+                    { "F", Color.Red }
+                };
+            }
+        }
+
+        /// <summary>
+        /// Calculate student's score.
+        /// </summary>
+        /// <returns>double student score</returns>
+        private double GetPoints()
+        {
+            // a vaiable to store the total score
+            double totalScore = 0;
+
+            // check if there is any scores in the array
+            if (scores.Length > 0)
+            {
+                foreach (var score in scores)
+                {
+                    // calculate the total score
+                    totalScore += score;
+                }
+                // calculate and return the points
+                return totalScore / scores.Length;
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Displays an exception in the MessageBox.
+        /// </summary>
+        /// <param name="exception">the current exception</param>
+        private void DisplayError(Exception exception)
+        {
+            // Display the error message and exit
+            MessageBox.Show($"An error has occured!\n" +
+                $"The error was: {exception.Message}");
+        }
+    }
+}
